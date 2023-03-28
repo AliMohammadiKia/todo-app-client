2336de89a41cbca6d3f6d2ac26a54656bcda91de
AliMohammadiKia <AliMohammadiKia01@gmail.com>
2023-03-28 12:46:27 +0330

    change todo size

--- src/components/todos/Todo.jsx
+++ src/components/todos/Todo.jsx
@@ -71,8 +71,8 @@ export const Todo = ({ todoProps }) => {
       borderRadius="0.25rem"
       sx={{
         width: {
-          xs: 220,
-          sm: 320,
+          xs: 280,
+          sm: 380,
           md: 520,
           lg: 620,
           xl: 720,
